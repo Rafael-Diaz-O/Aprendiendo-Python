ebd7e9ef601d3a57ffcef28d0257a40f2de6f92c
Rafael <rafadz@gmail.com>
2026-01-28 12:09:07 -0500

    introducion a tuplas

---
+++ 05_tuples.py
@@ -0,0 +1,19 @@
+ ##Tuples ##
+ 
+"""Una tupla es una estructura de datos en Python que 
+almacena una colección ordenada e inmutable de elementos. """
+
+#con inmutable se refiere a que no podra cambiar la info que sele coloque al inicio 
+ 
+my_tuple = tuple()
+my_other_turple = ()
+
+my_tuple = (19,1.76, "Rafael" , "Derek")
+print(my_tuple)
+print(type(my_tuple))
+
+print(my_tuple [0])
+print(my_tuple [-1])
+
+
+ 
